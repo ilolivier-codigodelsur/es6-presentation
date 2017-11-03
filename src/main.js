3562e06a7f96a5bb689edf2c793e95f68abbac04
Ivan L'olivier <ivan.lolivier@gmail.com>
2017-11-03 02:50:33 -0300

    Rest and spread

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
-function applyDiscount(cost, discount) {
-  discount = discount || 10;
-
-  return cost - (cost * discount / 100);
+/**************************************
+ REST
+ *******************/
+function sum(...numbers) {
+  return numbers.reduce(
+      (prev, current) => prev + current,
+  );
 }
+
+console.log('rest', sum(1, 2, 3));
+
+/**************************************
+ SPREAD
+ *******************/
+function sumTwo(x, y) {
+  return x + y;
+}
+
+let numbers = [1, 2];
+
+console.log('spread', sumTwo(...numbers));
